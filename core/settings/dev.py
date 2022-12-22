871fb7cabd0eb4616b3fcd3952dd5cd853b29fde
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2022-12-22 10:52:55 +0300

    ocean

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     pass
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = False
 DATABASES = {
     'default': {
@@ -28,3 +28,13 @@ DATABASES = {
 #SECURE_HSTS_SECONDS= 31536000
 #SECURE_HSTS_PRELOAD = True
 #SECURE_HSTS_INCLUDE_SUBDOMAINS=True
+# Note: for now, we are using the AWS naming-convention from Boto3
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "sfo3")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_LOCATION = os.getenv("AWS_LOCATION", "static")
+PUBLIC_MEDIA_LOCATION = os.getenv("PUBLIC_MEDIA_LOCATION", "media")
+AWS_DEFAULT_ACL = "public-read"
+AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
--- navigation/models.py
+++ navigation/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from wagtail.admin.panels import FieldPanel
-from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.core.fields import StreamField
 
 from .blocks import (
@@ -12,14 +12,14 @@ from .blocks import (
 
 
 @register_setting
-class NavigationMenuSetting(BaseGenericSetting):
+class NavigationMenuSetting(BaseSetting):
     items = StreamField(
         [
             ("internal_page", NavigationPageChooserBlock()),
             ("external_link", NavigationExternalLinkBlock()),
             ("drop_down", NavigationDropdownMenuBlock()),
-        ]
-        ,use_json_field=True,
+        ],
+        use_json_field=True,
     )
 
     panels = [
